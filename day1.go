d7da5946e232e2efb426175cc05d19a38067fd3b
ronaltama <tama.ronal@gmail.com>
2026-01-09 11:12:02 +0700

    update

---
+++ day1.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+var nama string = "ronaltama"
+var umur int = 21
+var kuliah bool = true
+
+func main ()  {
+	fmt.Println("halooo nama saya:", nama , "umur saya:", umur)
+}
